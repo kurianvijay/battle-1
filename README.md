a83068f1acdfc13e370c8e015723d15c1d195dea
Student <magnusarbuthnott@gmail.com>
2019-05-15 17:51:36 +0100

    added enter_names feature test, POST redirect Loop by creating a get /play route

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ As two Players,
 So we can play a personalised game of Battle,
 We want to Start a fight by entering our Names and seeing them
 
+Object | Message
+fight(new instance of Battle.new)  | start --> method
+name   | view name --> hash 
+
 As Player 1,
 So I can see how close I am to winning
 I want to see Player 2's Hit Points
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,23 @@
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit ('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Dave vs. Mittens'
+  end
+
+  # scenario 'storing player names' do
+  #   visit ('/')
+  #   fill_in :player_1_name, with: 'Dave'
+  #   fill_in :player_2_name, with: 'Mittens'
+  #   click_button 'Submit'
+
+
+
+
+
+
+
+
+end
